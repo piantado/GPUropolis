d6294ab385f2aaa295fed525ba5c37c1121ed034
Steven Piantadosi <spiantado@gmail.com>
2017-03-12 10:45:02 -0400

    removed old bad version

--- data-sources/NIST/BoxBOD/F.py
+++
@@ -1,8 +0,0 @@
-# Included fit function
-
-from math import exp
-
-def f(x):
-	b1 = 2.1380940889E+02
-	b2 = 5.4723748542E-01
-	return b1*(1-exp(-b2*x))
--- data-sources/NIST/Eckerle4/F.py
+++
@@ -1,9 +0,0 @@
-# Included fit function
-
-from math import exp
-
-def f(x):
-	b1 = 1.5543827178E+00
-	b2 = 4.0888321754E+00
-	b3 = 4.5154121844E+02
-	return (b1/b2) * exp(-0.5*((x-b3)/b2)**2)
